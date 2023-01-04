0391410b51c7dda4266f45f3c1948b21d14eb610
Junek3 <rlarudrms28@gmail.com>
2023-01-04 21:59:01 +0900

    Jasypt 걷어내고 IntelliJ 환경변수 사용하기

---
+++ ch8/src/main/java/com/rootlab/ch8/Ch8Application.java
@@ -0,0 +1,11 @@
+package com.rootlab.ch8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch8Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Ch8Application.class, args);
+	}
+}
