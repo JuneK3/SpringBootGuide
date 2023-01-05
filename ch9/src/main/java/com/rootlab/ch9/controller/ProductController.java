2e73881d9167b9383cbb60cb751d92c6c327354a
Junek3 <rlarudrms28@gmail.com>
2023-01-05 20:09:34 +0900

    ch9 준비

---
+++ ch9/src/main/java/com/rootlab/ch9/controller/ProductController.java
@@ -0,0 +1,49 @@
+package com.rootlab.ch9.controller;
+
+import com.rootlab.ch9.data.dto.ChangeProductNameDto;
+import com.rootlab.ch9.data.dto.ProductRequestDto;
+import com.rootlab.ch9.data.dto.ProductResponseDto;
+import com.rootlab.ch9.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+	private final ProductService productService;
+
+	@Autowired
+	public ProductController(ProductService productService) {
+		this.productService = productService;
+	}
+
+	@GetMapping()
+	public ResponseEntity<ProductResponseDto> getProduct(Long number) {
+		ProductResponseDto productResponseDto = productService.getProduct(number);
+		return ResponseEntity.status(HttpStatus.OK).body(productResponseDto);
+	}
+
+	@PostMapping()
+	public ResponseEntity<ProductResponseDto> postProduct(@RequestBody ProductRequestDto productRequestDto) {
+		ProductResponseDto productResponseDto = productService.saveProduct(productRequestDto);
+		return ResponseEntity.status(HttpStatus.CREATED).body(productResponseDto);
+	}
+
+	@PutMapping()
+	public ResponseEntity<ProductResponseDto> changeProductName(@RequestBody ChangeProductNameDto changeProductNameDto) {
+		ProductResponseDto productResponseDto = productService.changeProductName(
+				changeProductNameDto.getNumber(),
+				changeProductNameDto.getName()
+		);
+		return ResponseEntity.status(HttpStatus.ACCEPTED).body(productResponseDto);
+	}
+
+	@DeleteMapping(produces = "text/plain;charset=utf-8")
+	public ResponseEntity<String> deleteProduct(Long number) {
+		productService.deleteProduct(number);
+		return ResponseEntity.status(HttpStatus.OK).body("정상적으로 삭제되었습니다.");
+	}
+
+}
