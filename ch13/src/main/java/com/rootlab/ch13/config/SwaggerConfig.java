ec8f6f97df02a38339a470d65e77f2144a681bf5
Junek3 <rlarudrms28@gmail.com>
2023-01-27 05:07:10 +0900

    Swagger에서 Request의 Content-Type이 지정되지 않는 문제 해결

--- ch13/src/main/java/com/rootlab/ch13/config/SwaggerConfig.java
+++ ch13/src/main/java/com/rootlab/ch13/config/SwaggerConfig.java
@@ -14,52 +14,65 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @Configuration
 @EnableWebMvc
 public class SwaggerConfig {
-
-	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.OAS_30)
-				.securityContexts(Arrays.asList(securityContext()))
-				.securitySchemes(Arrays.asList(apiKey()))
-				.apiInfo(apiInfo())
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
+                .consumes(getConsumeContentTypes())
+                .produces(getProduceContentTypes())
+                .apiInfo(apiInfo())
 //				.useDefaultResponseMessages(false)
-				.select()
-				.apis(RequestHandlerSelectors.basePackage("com.rootlab.ch13.controller"))
-				.paths(PathSelectors.any())
-				.build();
-	}
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.rootlab.ch13.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
 
-	private ApiInfo apiInfo() {
-		return new ApiInfoBuilder()
-				.title("Spring Boot Open API Test with Swagger")
-				.description("description")
-				.version("1.0.0")
-				.build();
-	}
+    private Set<String> getConsumeContentTypes() {
+        Set<String> consumes = new HashSet<>();
+        consumes.add("application/json;charset=UTF-8");
+        consumes.add("application/x-www-form-urlencoded");
+        return consumes;
+    }
 
-	// https://gksdudrb922.tistory.com/219
-	// Spring Security + Swagger 3.0 적용
-	// TODO: 코드가 의미하는게 무엇인지 제대로 파악하기
+    private Set<String> getProduceContentTypes() {
+        Set<String> produces = new HashSet<>();
+        produces.add("application/json;charset=UTF-8");
+        return produces;
+    }
 
-	private SecurityContext securityContext() {
-		return SecurityContext.builder()
-				.securityReferences(defaultAuth())
-				.build();
-	}
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot Open API Test with Swagger")
+                .description("description")
+                .version("1.0.0")
+                .build();
+    }
 
-	private List<SecurityReference> defaultAuth() {
-		AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
-		AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-		authorizationScopes[0] = authorizationScope;
-		return Arrays.asList(new SecurityReference("Authorization", authorizationScopes));
-	}
+    // https://gksdudrb922.tistory.com/219
+    // Spring Security + Swagger 3.0 적용
+    // TODO: 코드가 의미하는게 무엇인지 제대로 파악하기
 
-	private ApiKey apiKey() {
-		return new ApiKey("Authorization", "Authorization", "header");
-	}
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("Authorization", authorizationScopes));
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("Authorization", "Authorization", "header");
+    }
 }
